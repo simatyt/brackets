d1d5234cf4ba5bf45ba8840eb16ae7a4470381d4
Kostantin Simonenko <kostantin_simonenko@konstansins-mbp.ksim>
2020-10-11 00:36:16 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = parseConfig(bracketsConfig);
+  let s = str;
+  while (containsAny(s, brackets)) {
+    s = removeEntries(s, brackets);
+  }
+  return s.length === 0;
+}
+
+function containsAny(str, substrings) {
+  for (let sub of substrings) if (str.includes(sub)) return true;
+  return false;
+}
+
+function removeEntries(str, substrings) {
+  let tmp = str;
+  for (let s of substrings) tmp = tmp.split(s).join("");
+  return tmp;
+}
+
+function parseConfig(brackets) {
+  return brackets.map(pair => pair[0] + pair[1]);
 }
